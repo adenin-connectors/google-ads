567e0e6323a10abf8160b31976a83786b94cf3c6
olliejm <olliejm@protonmail.com>
2019-04-12 10:28:25 +0100

    prepare for azure pipeline

--- activities/common/api.js
+++ activities/common/api.js
@@ -1,75 +1,10 @@
 'use strict';
+
 const got = require('got');
 const HttpAgent = require('agentkeepalive');
 const HttpsAgent = HttpAgent.HttpsAgent;
 
-function api(path, opts) {
-  if (typeof path !== 'string') {
-    return Promise.reject(new TypeError(`Expected \`path\` to be a string, got ${typeof path}`));
-  }
-
-  opts = Object.assign({
-    json: false,
-    token: Activity.Context.connector.token,
-    endpoint: 'https://adwords.google.com',
-    method: 'POST',
-    agent: {
-      http: new HttpAgent(),
-      https: new HttpsAgent()
-    }
-  }, opts);
-
-  opts.headers = Object.assign({
-    'clientCustomerId': Activity.Context.connector.custom1,
-    'developerToken': Activity.Context.connector.custom2,
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'user-agent': 'adenin Now Assistant Connector, https://www.adenin.com/now-assistant'
-  }, opts.headers);
-
-  if (opts.token) {
-    opts.headers.Authorization = `Bearer ${opts.token}`;
-  }
-
-  opts.form = true;
-  opts.body = {
-    ...opts.body,
-    __rdxml: xmlDefinition
-  };
-
-  const url = /^http(s)\:\/\/?/.test(path) && opts.endpoint ? path : opts.endpoint + path;
-
-  if (opts.stream) {
-    return got.stream(url, opts);
-  }
-
-  return got(url, opts).catch(err => {
-    throw err;
-  });
-}
-
-const helpers = [
-  'get',
-  'post',
-  'put',
-  'patch',
-  'head',
-  'delete'
-];
-
-api.stream = (url, opts) => apigot(url, Object.assign({}, opts, {
-  json: false,
-  stream: true
-}));
-
-for (const x of helpers) {
-  const method = x.toUpperCase();
-  api[x] = (url, opts) => api(url, Object.assign({}, opts, { method }));
-  api.stream[x] = (url, opts) => api.stream(url, Object.assign({}, opts, { method }));
-}
-
-module.exports = api;
-
-let xmlDefinition = `
+const xmlDefinition = `
 <reportDefinition xmlns="https://adwords.google.com/api/adwords/cm/v201809">
 <selector>
 <fields>CampaignId</fields>
@@ -88,3 +23,65 @@ let xmlDefinition = `
 <dateRangeType>YESTERDAY</dateRangeType>
 <downloadFormat>CSV</downloadFormat>
 </reportDefinition>`;
+
+function api(path, opts) {
+  if (typeof path !== 'string') {
+    return Promise.reject(new TypeError(`Expected \`path\` to be a string, got ${typeof path}`));
+  }
+
+  opts = Object.assign({
+    json: false,
+    token: Activity.Context.connector.token,
+    endpoint: 'https://adwords.google.com',
+    method: 'POST',
+    agent: {
+      http: new HttpAgent(),
+      https: new HttpsAgent()
+    }
+  }, opts);
+
+  opts.headers = Object.assign({
+    clientCustomerId: Activity.Context.connector.custom1,
+    developerToken: Activity.Context.connector.custom2,
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'user-agent': 'adenin Now Assistant Connector, https://www.adenin.com/now-assistant'
+  }, opts.headers);
+
+  if (opts.token) opts.headers.Authorization = `Bearer ${opts.token}`;
+
+  opts.form = true;
+  opts.body = {
+    ...opts.body,
+    __rdxml: xmlDefinition
+  };
+
+  const url = /^http(s)\:\/\/?/.test(path) && opts.endpoint ? path : opts.endpoint + path;
+
+  if (opts.stream) return got.stream(url, opts);
+
+  return got(url, opts).catch((err) => {
+    throw err;
+  });
+}
+
+const helpers = [
+  'get',
+  'post',
+  'put',
+  'patch',
+  'head',
+  'delete'
+];
+
+api.stream = (url, opts) => got(url, Object.assign({}, opts, {
+  json: false,
+  stream: true
+}));
+
+for (const x of helpers) {
+  const method = x.toUpperCase();
+  api[x] = (url, opts) => api(url, Object.assign({}, opts, {method}));
+  api.stream[x] = (url, opts) => api.stream(url, Object.assign({}, opts, {method}));
+}
+
+module.exports = api;
