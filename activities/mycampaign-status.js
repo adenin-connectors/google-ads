24236c668bcd1095ac01da964faeba537e62c10a
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-04-19 14:14:09 +0200

    Removed global variable Activity.

--- activities/mycampaign-status.js
+++ activities/mycampaign-status.js
@@ -1,11 +1,41 @@
 'use strict';
 const api = require('./common/api');
 
+const xmlDefinition = `
+<reportDefinition xmlns="https://adwords.google.com/api/adwords/cm/v201809">
+<selector>
+<fields>CampaignId</fields>
+<fields>Impressions</fields>
+<fields>Clicks</fields>
+<fields>Cost</fields>
+<predicates>
+  <field>AdGroupStatus</field>
+  <operator>IN</operator>
+  <values>ENABLED</values>
+  <values>PAUSED</values>
+</predicates>
+</selector>
+<reportName>Custom Adgroup Performance Report</reportName>
+<reportType>ADGROUP_PERFORMANCE_REPORT</reportType>
+<dateRangeType>YESTERDAY</dateRangeType>
+<downloadFormat>CSV</downloadFormat>
+</reportDefinition>`;
+
 module.exports = async (activity) => {
   try {
-    const response = await api(`/api/adwords/reportdownload/v201809`);
+    api.initialize(activity);
 
-    if (Activity.isErrorResponse(response)) return;
+    let opts = {
+      method: 'POST'
+    };
+    opts.form = true;
+    opts.body = {
+      ...opts.body,
+      __rdxml: xmlDefinition
+    };
+    const response = await api(`/api/adwords/reportdownload/v201809`, opts);
+
+    if ($.isErrorResponse(activity, response)) return;
 
     //splits string after 'total' to get totals of Impressions,Clicks And cost in that order
     let data = response.body.split('Total,')[1];
@@ -16,10 +46,10 @@ module.exports = async (activity) => {
     let totalCost = values[2];
 
     let campaignStatus = {
-      title: T('Camapaigns Status'),
+      title: T(activity, 'Camapaigns Status'),
       link: 'https://ads.google.com/aw/overview?',
-      linkLabel: T('All Campaigns'),
-      description: T(`Yesterday you had {0} clicks and {1} impression with a total cost of {2}$.`, totalClicks, totalImpressions, totalCost / 1000000),
+      linkLabel: T(activity, 'All Campaigns'),
+      description: T(activity, `Yesterday you had {0} clicks and {1} impression with a total cost of {2}$.`, totalClicks, totalImpressions, totalCost / 1000000),
       color: 'blue',
       value: response.body.length,
       actionable: true
@@ -27,6 +57,6 @@ module.exports = async (activity) => {
 
     activity.Response.Data = campaignStatus;
   } catch (error) {
-    Activity.handleError(error);
+    $.handleError(activity, error);
   }
 };
